bebf6702c17a6a440853449723089e5d0ed564df
Adrien Gallou <adriengallou@gmail.com>
2012-04-07 00:35:02 +0200

    factorised rebuild branch api action with new base api action

--- apps/api/modules/branch/actions/rebuildAction.class.php
+++ apps/api/modules/branch/actions/rebuildAction.class.php
@@ -4,39 +4,30 @@ class rebuildAction extends baseApiJenkinsAction
 {
 
   /**
+   *
    * @param sfWebRequest $request
    *
+   * @return array
    */
-  public function execute($request)
+  public function getContent($request)
   {
     $branchName = $request->getParameter('git_branch_slug');
     $userId     = $this->getUser()->getUserId();
 
-    try
-    {
-      $groupRun   = JenkinsGroupRunPeer::retrieveBySfGuardUserIdAndGitBranchSlug($userId, $branchName);
+    $groupRun   = JenkinsGroupRunPeer::retrieveBySfGuardUserIdAndGitBranchSlug($userId, $branchName);
 
-      if (null === $groupRun)
-      {
-        throw new RuntimeException(sprintf('Can\'t retrieve JenkinsGroupRun with branch name %s and user id %s', $branchName, $userId));
-      }
-
-      $groupRun->rebuild($this->getJenkins(), $request->getParameter('delayed') == 1);
-      JenkinsRunPeer::fillEmptyJobBuildNumber($this->getJenkins(), $this->getUser()->getUserId());
-      $message = $request->getParameter('delayed') == 1 ? 'added to delay list' : 'relaunched';
-      $return = array(
-        'status'  => 0,
-        'message' => sprintf('The build [%s] has been %s', $groupRun->getLabel(), $message),
-      );
-    }
-    catch (Exception $e)
+    if (null === $groupRun)
     {
-      $return = array(
-        'status'  => 1,
-        'message' => $e->getMessage(),
-      );
+      throw new RuntimeException(sprintf('Can\'t retrieve JenkinsGroupRun with branch name %s and user id %s', $branchName, $userId));
     }
-    return $this->renderText(json_encode($return));
+
+    $groupRun->rebuild($this->getJenkins(), $request->getParameter('delayed') == 1);
+    JenkinsRunPeer::fillEmptyJobBuildNumber($this->getJenkins(), $this->getUser()->getUserId());
+    $message = $request->getParameter('delayed') == 1 ? 'added to delay list' : 'relaunched';
+
+    return array(
+      'message' => sprintf('The build [%s] has been %s', $groupRun->getLabel(), $message),
+    );
   }
 
 }
