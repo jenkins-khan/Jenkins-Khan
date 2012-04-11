db10454d9c05febcd457ec756788a0bd4234841d
Sébastien Rogier <sebrogier@gmail.com>
2012-04-11 14:01:14 +0200

    do not display Jenkins' url when Jenkins is not available
    create a btnGroupComponent to factorize btn-group and clean url creation

--- apps/front/modules/jenkins/actions/listGroupRunComponent.class.php
+++ apps/front/modules/jenkins/actions/listGroupRunComponent.class.php
@@ -22,8 +22,8 @@ class listGroupRunComponent extends sfComponent
     $durationFormatter     = new durationFormatter();
     $runs                  = $currentGroup->getJenkinsRuns();
     $dataRuns              = array();
-    $isGroupRunRebuildable = false;
-
+    $isJenkinsAvailable    = $jenkins->isAvailable();
+    
     foreach ($runs as $run)
     {
       $isCancelable = false;
@@ -31,7 +31,7 @@ class listGroupRunComponent extends sfComponent
       $remaining    = null;
       $isRunning    = false;
       $urlBuild     = $run->getUrlBuild($jenkins);
-      if (!$jenkins->isAvailable())
+      if (!$isJenkinsAvailable)
       {
         $isCancelable = false;
       }
@@ -47,12 +47,6 @@ class listGroupRunComponent extends sfComponent
         $isCancelable = true;
       }
 
-      if ($run->isRebuildable())
-      {
-        $isGroupRunRebuildable = true;
-      }
-
-
       /** @var JenkinsRun $run */
       $dataRuns[$run->getId()] = array(
         'job_name'            => $run->getJobName(),
@@ -67,23 +61,111 @@ class listGroupRunComponent extends sfComponent
         'is_cancelable'       => $isCancelable,
         'url'                 => $urlBuild,
         'url_console_log'     => $urlBuild . '/console',
-        'url_test_report'     => $urlBuild . '/testReport',
-        'url_remove'          => $this->generateUrl('run_remove', $run),
         'url_rebuild'         => $this->generateUrl('run_rebuild', $run),
-        'url_rebuild_delayed' => !$isRunning && $run->isRebuildable() ? $this->generateUrl('run_rebuild_delayed', $run) : false,
+        'dropdown_links'      => $this->buildDropdownLinksJenkinsRun($run, $jenkins, $isRunning, $isJenkinsAvailable),
+        'title_url_rebuild'   => $isRunning ? "Cancel current build and relaunch" : (!$run->isRebuildable() ? "Launch build immediatly" : "Relaunch build")
       );
     }
 
-    $this->setVar('is_group_run_rebuildable', $isGroupRunRebuildable);
+    $currentGroupInfos = array(
+      'id'              => $currentGroup->getId(),
+      'git_branch'      => $currentGroup->getGitBranch(),
+      'url_add_build'   => $isJenkinsAvailable ? 'jenkins/addBuild?group_run_id='.$currentGroup->getId() : null,
+      'dropdown_links'  => $this->buildDropdownCurrentGroup($currentGroup, $isJenkinsAvailable),
+    );
+    
     $this->setVar('runs', $dataRuns);
-    $this->setVar('current_group_run', array(
-      'id'              => null === $currentGroup ? null : $currentGroup->getId(),
-      'git_branch'      => null === $currentGroup ? null : $currentGroup->getGitBranch(),
-      'git_branch_slug' => null === $currentGroup ? null : $currentGroup->getGitBranchSlug(),
-      'url_add_build'   => null === $currentGroup ? null : 'jenkins/addBuild?group_run_id='.$currentGroup->getId(),
-      'url_duplicate'   => null === $currentGroup ? null : 'jenkins/createGroupRun?from_group_run_id=' . $currentGroup->getId(),
-      'url_delete'      => null === $currentGroup ? null : 'jenkins/deleteGroupRun?id=' . $currentGroup->getId(),
-    ));
+    $this->setVar('current_group_run', $currentGroupInfos);
   }
 
+
+  /**
+   * @param \JenkinsRun $run
+   * @param \Jenkins    $jenkins
+   * @param             $isRunning
+   * @param             $isJenkinsAvailable
+   *
+   * @return array
+   */
+  private function buildDropdownLinksJenkinsRun(JenkinsRun $run, Jenkins $jenkins, $isRunning, $isJenkinsAvailable)
+  {
+    $links    = array();
+    $urlBuild = $run->getUrlBuild($jenkins);
+
+    if ($isRunning && $run->isRebuildable())
+    {
+      $links[] = array(
+        'label' => 'Delay',
+        'title' => 'Relaunch build (delayed)',
+        'url'   => $this->generateUrl('run_rebuild_delayed', $run),
+      );
+    }
+
+    $links[] = array(
+      'label' => 'Remove build',
+      'title' => 'Remove build from build branch',
+      'url'   => $this->generateUrl('run_remove', $run),
+    );
+
+    $isJenkinsAvailable && $links[] = array(
+      'label'     => 'Go to console log',
+      'title'     => 'View Jenkins console log',
+      'url'       => $urlBuild . '/console',
+      'options'   => array(
+        'class'  => 'jenkins',
+        'target' => '_blank'
+      ),
+    );
+
+    $isJenkinsAvailable && $links[] = array(
+      'label'     => 'Go to test report',
+      'url'       => $urlBuild . '/testReport',
+      'options'   => array(
+        'class'  => 'jenkins',
+        'target' => '_blank'
+      ),
+    );
+
+    return $links;
+  }
+
+  /**
+   * @param \JenkinsGroupRun $currentGroup
+   * @param boolean          $isJenkinsAvailable
+   *
+   * @return array
+   */
+  private function buildDropdownCurrentGroup(JenkinsGroupRun $currentGroup, $isJenkinsAvailable)
+  {
+    $links = array();
+    $isJenkinsAvailable && $links[] = array(
+      'label' => 'Relaunch all jobs',
+      'url'   => $this->generateUrl('branch_rebuild', $currentGroup),
+    );
+
+    $links[] = array(
+      'label' => 'Add all jobs in delayed list',
+      'url'   => $this->generateUrl('branch_rebuild_delayed', $currentGroup),
+    );
+
+    $isJenkinsAvailable && $links[] = array(
+      'label' => 'Add a job',
+      'url'   => 'jenkins/addBuild?group_run_id=' . $currentGroup->getId(),
+      'title' => 'Add a job to this build branch',
+    );
+
+    $isJenkinsAvailable && $links[] = array(
+      'label' => 'Duplicate build branch',
+      'url'   => 'jenkins/createGroupRun?from_group_run_id=' . $currentGroup->getId(),
+    );
+
+    $links[] = array(
+      'label' => 'Delete build branch',
+      'url'   => 'jenkins/deleteGroupRun?id=' . $currentGroup->getId(),
+    );
+    
+    return $links;
+  }
+  
+  
 }
